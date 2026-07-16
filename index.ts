555b0eff63a694f67012bfb37c8ccd1382e44c86
glckfndr <obulan65@gmail.com>
2026-07-16 12:06:08 +0300

    config: run front and back from root concurrently

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+concurrently(
+  [
+    { name: "server", command: "bun run dev", cwd: "packages/server", prefixColor: "green" },
+    { name: "client", command: "bun run dev", cwd: "packages/client", prefixColor: "blue"  }
+  ]
+);
