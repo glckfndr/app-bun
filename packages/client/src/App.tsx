4f83e7f0539c0bc01672b01e8d46fbc3fa09096d
glckfndr <obulan65@gmail.com>
2026-07-17 09:43:35 +0300

    config: install husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -16,10 +16,10 @@ function App() {
 
    return (
       <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button> Click me</Button>
+                <p className="font-bold text-3xl">{message}</p>
+         <Button> Click me!</Button>
       </div>
-   );
+   )
 }
 
 export default App;
